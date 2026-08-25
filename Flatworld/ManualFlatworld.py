ce683c08f784595f300e2f99ceaf4cd8b1aa054e
bowerjohnd <bowerjohnd@gmail.com>
2026-08-25 13:25:47 -0400

    Chapter 4 - Flatworld - initial game setup

---
+++ Flatworld/ManualFlatworld.py
@@ -0,0 +1,15 @@
+from Flatworld import *
+
+# ----- Main Program -----
+
+pygame.init()
+
+ticks = mainLoop(
+            initialSpawns = {Red: 4,
+                             Player: 1,
+                             Grass: 20},
+            spawnInterval = 30,
+            periodicSpawns = {Grass: 1},
+            boardSize = (750,500), playerType = Blue)
+
+
